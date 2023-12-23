022843d8a9f8766479d85cb87e5ca1335ab456d7
Oleksandr Chornyi <chornyi.oleksandr@lll.kpi.ua>
2023-12-23 16:18:16 +0100

    Setup games service

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+import { join } from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'users',
+        protoPath: join(__dirname, 'users.proto'),
+        url: '0.0.0.0:5001',
+      },
+    },
+  );
+  await app.listen();
+}
+
+bootstrap();
